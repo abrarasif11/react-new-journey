641e87dde6ca9688ac6bc1d2cb6e49514ac80154
abrarasif11 <faasif929@gmail.com>
2025-07-24 12:36:21 +0600

    core-concept

---
+++ basic-react/src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Let's Begin TAANDOB</h1>
+      <Person/>
+      <Student/>
+    </>
+  )
+}
+function Person (){
+  const age = 12;
+  const sport = {fav:'football'};
+  return <h2>MY fav is {sport.fav} with age :{age} years old</h2>
+}
+function Student (){
+  const Name = 'messi';
+  const Club = 'miami';
+  return <h1>Name {Name} is playing {Club} club</h1>
+}
+export default App
