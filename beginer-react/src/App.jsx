18fc463d59feafea0a088b4058f1f71ba349ce9d
abrarasif11 <faasif929@gmail.com>
2025-07-28 19:48:34 +0600

    event-handle

---
+++ beginer-react/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+function handleClick(){
+  alert("Thanks for Clicking")
+}
+  return (
+    <>
+     <h2>beginer react</h2>
+     <button onClick={handleClick}>click me</button>
+    </>
+  )
+}
+
+export default App
