b5d440ef811132af03aa1698674d1da7d0bc760b
abrarasif11 <faasif929@gmail.com>
2025-07-31 20:14:17 +0600

    no-coutry-data-on-api

--- react-world-tour/src/App.jsx
+++ react-world-tour/src/App.jsx
@@ -1,12 +1,18 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 import Countries from './Countries'
 
 function App() {
-  
+  const [countries,setCountries] = useState([]);
+  useEffect(() =>{
+    fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) =>setCountries(data))
+  },[])
   return (
     <>
-      <h1>React World Tour</h1>
+      <h1>React World Tour : {countries.length}</h1>
       <Countries/>
     </>
   )
